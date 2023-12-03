b9946c56a3e90225216b51e3b3f097b0b4df61de
Luis Henrique <lhlimasan@gmail.com>
2023-12-03 10:48:03 -0300

    calculadora soma

--- Calculadora.java
+++ Calculadora.java
@@ -6,4 +6,12 @@ public class Calculadora {
     public int subtrair(int a, int b) {
         return a - b;
     }
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        int resultadoSoma = calculadora.somar(5, 3);
+        int resultadoSubtracao = calculadora.subtrair(10, 4);
+        System.out.println("Resultado da soma: " + resultadoSoma);
+        System.out.println("Resultado da subtração: " + resultadoSubtracao);
+    }
 }
---
+++ CalculadoraSoma.java
@@ -0,0 +1,9 @@
+public class CalculadoraSoma {
+        public int somar(int a, int b) {
+            return a + b;
+        }
+        
+        public int somar(int a, int b, int c) {
+            return a + b + c;
+        }
+    }
