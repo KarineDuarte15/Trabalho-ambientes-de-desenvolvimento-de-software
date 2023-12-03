24d1dc91d2346a6d2064a2fe807eca5436cfe52e
cleanyteixeira <cleanyteixeira02@gmail.com>
2023-12-03 11:25:30 -0300

    calculadora diivisão

--- CalculadoraSoma.java
+++ CalculadoraSoma.java
@@ -6,4 +6,12 @@ public class CalculadoraSoma {
         public int somar(int a, int b, int c) {
             return a + b + c;
         }
+
+        public static void main(String[] args) {
+            CalculadoraSoma calculadora = new CalculadoraSoma();
+            int resultadoSoma2 = calculadora.somar(2, 3);
+            int resultadoSoma3 = calculadora.somar(1, 2, 3);
+            System.out.println("Resultado da soma 2: " + resultadoSoma2);
+            System.out.println("Resultado da soma 3: " + resultadoSoma3);
+        }
     }
