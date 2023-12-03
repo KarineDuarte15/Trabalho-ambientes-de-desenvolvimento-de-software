3349815215e43731ff2b7efe395c6613a70f825c
Maria <mariajsousa@edu.unifor.br>
2023-12-03 18:32:06 -0300

    calculadora subtracao

--- CalculadoraPotencia.java
+++ CalculadoraPotencia.java
@@ -6,4 +6,12 @@ public class CalculadoraPotencia {
     public int calcularPotencia(int base, int expoente) {
         return (int) Math.pow(base, expoente);
     }
+
+     public static void main(String[] args) {
+        CalculadoraPotencia calculadora = new CalculadoraPotencia();
+        double resultadoPotenciaDouble = calculadora.calcularPotencia(2.0, 3.0);
+        int resultadoPotenciaInt = calculadora.calcularPotencia(3, 4);
+        System.out.println("Resultado da potência (double): " + resultadoPotenciaDouble);
+        System.out.println("Resultado da potência (int): " + resultadoPotenciaInt);
+    }
 }
